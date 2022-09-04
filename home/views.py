4128d1f2448c8d2c6b9490a6faa1e449614b88c6
Kenta Tanaka <tkenta2929@gmail.com>
2022-09-03 19:22:47 -0700

    frontend works

--- home/views.py
+++ home/views.py
@@ -1,42 +1,47 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 import random
 import json
 import torch
-from training.model import NeuralNet
-from training.nltk_utils import bag_of_words, tokenize
+from .training.model import NeuralNet
+from .training.nltk_utils import bag_of_words, tokenize
 
 
 
 # Create your views here.
 
 def home(request):
-
     context = {}
     return render(request, 'home.html', context)
 
-device = torch.device('cuba' if torch.cuda.is_available() else 'cpu')
-with open('training/intents.json', 'r') as json_data:
-    intents = json.load(json_data)
-FILE = 'training/data.pth'
-data = torch.load(FILE)
+def chatbot(request):
+    device = torch.device('cuba' if torch.cuda.is_available() else 'cpu')
+    with open('/Users/kentatanaka/MyProjects/chatbot/home/training/intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+    FILE = '/Users/kentatanaka/MyProjects/chatbot/home/training/data.pth'
+    data = torch.load(FILE)
 
-input_size = data['input_size']
-hidden_size = data['hidden_size']
-output_size = data['output_size']
-all_words = data['all_words']
-tags = data['tags']
-model_state = data['model_state']
+    input_size = data['input_size']
+    hidden_size = data['hidden_size']
+    output_size = data['output_size']
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data['model_state']
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
 
-bot_name = "Boty"
-print('lets have a chat')
+    # getting fetching input from frontend
+    data = json.loads(request.body)
+    input = data['input'].lower()
+    
+    sentence = input
 
-while True:
-    sentence = input("You: ")
+    if sentence =='q':
+        return JsonResponse({'response': 'Bye!'})
+
     sentence = tokenize(sentence)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -52,6 +57,13 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                    # print(f"{bot_name}: {random.choice(intent['responses'])}")
+                    response = random.choice(intent['responses'])
     else:
-        print(f"{bot_name}: I do not understand.. ")
+        response = 'I do not understand...'
+        # print(f"{bot_name}: I do not understand.. ")
+
+    # storing calculated response to variable "response"
+    # response = random.choice(intent['responses'])
+
+    return JsonResponse({'response': response})
